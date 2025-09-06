050abe7485a46de260d7b1dc8c81812af8be4090
bhavyachappa12-ui <bhavyachappa12@gmail.com>
2025-09-06 12:33:40 +0530

    Learned datatypes in js

--- practice.js
+++ practice.js
@@ -1,1 +1,34 @@
 console.log("Hello World,Bhavya")
+
+let name = "Bhavya Chappa";
+console.log(name)
+
+let num = 18;
+console.log(num)
+
+let isLogged=false;
+console.log(isLogged)
+
+let x;
+console.log(x)
+
+let y=null;
+console.log(y)
+
+console.log(typeof name)
+console.log(typeof num)
+console.log(typeof isLogged)
+console.log(typeof x)
+console.log(typeof y)
+
+let arr=[1,2,3,4,5]
+console.log(arr[0])
+console.log(typeof arr)
+
+let person={
+    name:"Bhavya",
+    age:19
+}
+console.log(person.name)
+console.log(person.age)
+console.log(typeof person)
